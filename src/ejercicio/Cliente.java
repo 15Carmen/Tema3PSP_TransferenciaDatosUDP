f500d620161b23c5ba800a36e14c89aa323e5d2d
15Carmen <carmen.martin@iesnervion.es>
2023-02-07 12:01:28 +0100

    :]

--- src/ejercicio/Cliente.java
+++ src/ejercicio/Cliente.java
@@ -7,35 +7,21 @@ import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Cliente {
+    private static DatagramSocket socket;
+    private static DatagramPacket packet;
+
     public static void main(String[] args) {
 
         try {
-
             //Obtener direccion IP local
             InetAddress direccion = InetAddress.getLocalHost();
 
             //Creacion del socket
             System.out.println("Creacion del socket");
-            DatagramSocket socket = new DatagramSocket(41500);
+            socket = new DatagramSocket(41500);
 
-            String mensaje;
-
-            for (int i = 0; i <= 9999; i++) {
-                //Creacion del mensaje
-                mensaje = "Mensaje: " + i;
-                byte[] buffer = mensaje.getBytes();
-
-                System.out.println("Creamos el datagrama");
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, direccion, 41700);
-                socket.send(packet);
-            }
-
-            mensaje= "FIN";
-            byte[] buffer = mensaje.getBytes();
-
-            System.out.println("Creamos el datagrama");
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, direccion, 41700);
-            socket.send(packet);
+            //Creamos el mensaje
+            creacionMensaje(direccion);
 
         } catch (SocketException e) {
             System.err.println("Error en la creacion del socket");
@@ -43,4 +29,35 @@ public class Cliente {
             System.err.println("Error en la recepcion del paquete");
         }
     }
+
+    private static void creacionMensaje(InetAddress direccion) {
+        //Declaramos las variables
+        String mensaje;
+        byte[] buffer;
+
+        try {
+            for (int i = 0; i <= 9999; i++) {
+                //Creacion del mensaje
+                mensaje = "Mensaje: " + i;
+                buffer = mensaje.getBytes();
+
+                System.out.println("Creamos el datagrama");
+                packet = new DatagramPacket(buffer, buffer.length, direccion, 41700);
+                socket.send(packet);
+            }
+
+            mensaje = "FIN";
+            buffer = mensaje.getBytes();
+
+            System.out.println("Creamos el datagrama");
+            packet = new DatagramPacket(buffer, buffer.length, direccion, 41700);
+            socket.send(packet);
+
+        } catch (IOException e) {
+            System.err.println("Error al enviar el paquete");
+        }
+
+    }
+
+
 }
--- src/ejercicio/Servidor.java
+++ src/ejercicio/Servidor.java
@@ -1,5 +1,8 @@
 package ejercicio;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -9,33 +12,47 @@ import java.net.SocketException;
 public class Servidor {
     public static void main(String[] args) {
 
+        //Declaramos las variables
+        DatagramSocket socket;
+        DatagramPacket packet;
+        BufferedWriter bw;
+        String mensaje = "";
+        InetAddress direccion;
+        byte[] buffer;
+
         try {
             //Obtener direccion IP local
-            InetAddress direccion = InetAddress.getLocalHost();
+            direccion = InetAddress.getLocalHost();
 
             //Creacion del socket
             System.out.println("Creacion del socket");
-            DatagramSocket socket = new DatagramSocket(41700);
+            socket = new DatagramSocket(41700);
+
+            bw = new BufferedWriter(new FileWriter("src/ejercicio/fichero.txt"));
 
             while (true){
 
-                String mensaje = "";
-
-                while (!mensaje.equals("FIN")){
-                    byte[] buffer = new byte[64];
+                while (!mensaje.equals("FIN")){     //Mientras que mensaje sea distinto de la palabra "FIN"
+                    buffer = new byte[64];
                     System.out.println();
                     System.out.println("Creamos el datagrama");
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                    packet = new DatagramPacket(buffer, buffer.length);
                     System.out.println("A la espera de recibir datagrama");
                     socket.receive(packet);
 
                     System.out.println();
                     System.out.println("Leemos el mensaje");
-                    mensaje = new String(packet.getData());
+                    mensaje = new String(packet.getData()).trim();
                     System.out.println("Mensaje que ha enviado el cliente: " + mensaje.trim());
-                }
-            }
 
+                    bw.write(mensaje);  //Con este escribe en el fichero tal cual el mensaje de la consola
+                    //bw.write(mensaje.split(" ")[1]); Con esta línea de código se quita la parte escrita cuando se escribe en
+                    //el fichero, pero salta una excepción y deja de escribir en el fichero alrededor del número 8376
+                    bw.newLine();
+                }
+                //Cerramos el bw
+                bw.close();
+            }
             
         } catch (
                 SocketException e) {
@@ -45,4 +62,5 @@ public class Servidor {
             System.err.println("Error en la recepcion del paquete");
         }
     }
+
 }
